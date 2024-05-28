73e7edddd0b80a8dce9e7f2a562eda6fa95e6e87
Sahil Patil <sahilpat090@gmail.com>
2024-05-28 23:13:24 +0530

    first commit

---
+++ Client/src/components/CreateTaskModule.jsx
@@ -0,0 +1,5 @@
+function CreateTaskModule() {
+  return <div></div>;
+}
+
+export default CreateTaskModule;
---
+++ Client/src/components/UpdateTaskModule.jsx
@@ -0,0 +1,5 @@
+function UpdateTaskModule() {
+  return <div></div>;
+}
+
+export default UpdateTaskModule;
